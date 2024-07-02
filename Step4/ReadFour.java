dc3bf426c325dea3cdce5bb9ec6866b8fe6cd113
Josue <josue@Zipcoders-MacBook-Pro-10.local>
2024-07-02 15:20:15 -0400

    Finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,18 +1,35 @@
 package Step4;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadFour
-{
+public class ReadFour {
     /**
-     * Yes, you can look at the other 
+     * Yes, you can look at the other
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
+    public static void main(String[] args) {
         {
+
+            try {
+                Scanner fileIn = new Scanner(new File("Step4/input.txt"));
+                int sum = 0;
+                while (fileIn.hasNext()) {
+                    // Reads the entire line
+                    int lineIn = fileIn.nextInt();
+                    // Output the line
+                    sum += lineIn;
+                }
+                System.out.println(sum);
+                fileIn.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found.");
+                throw new RuntimeException(e);
+            }
             // Print out a running total of all the
             // values in the input file.
         }
+    }
 }
